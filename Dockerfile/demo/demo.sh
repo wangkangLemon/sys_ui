867ef82011f48908996211c9d4bfd64b76b3b779
dangzihao <dangzh@vodjk.com>
2017-11-10 15:51:28 +0800

    [0.0.0.1] 打包docker镜像

--- Dockerfile/demo/demo.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-cd /node/src/git.vodjk.com/yst/provider-ui
-npm run start
---
+++ Dockerfile/dist/static/js/33.c48e39064030e06d8084.js
@@ -0,0 +1,2 @@
+webpackJsonp([33],{161:function(e,t,r){var n=r(10)(r(260),r(391),null,null);e.exports=n.exports},260:function(e,t,r){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.default={}},391:function(e,t){e.exports={render:function(){var e=this,t=e.$createElement,r=e._self._c||t;return r("keep-alive",{attrs:{exclude:"sys-form, sys-video-add"}},[r("router-view")],1)},staticRenderFns:[]}}});
+//# sourceMappingURL=33.c48e39064030e06d8084.js.map
